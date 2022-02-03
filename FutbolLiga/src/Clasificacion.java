521b0d0495e61ef6410e619271036476032b82db
ivanmarquez99 <ivanaurelio.s258702@cesurformacion.com>
2022-02-03 13:49:12 +0100

    Clasificacion

--- FutbolLiga/src/Clasificacion.java
+++ FutbolLiga/src/Clasificacion.java
@@ -1,7 +1,59 @@
 
 public class Clasificacion {
-
+ 
+	/**
+	 * Contructor de Clasificacion
+	 */
+	public Clasificacion() {
+	 
+ }
+	/**
+	 *  Variables de la Clasificacion
+	 */
+	private Equipo equipo;   // Nombre de los equipos
+	private int victorias;   // Numero de victorias
+	private int derrotas;    // Numero de derrotas
+	private int golesfavor;  // Numero de goles a favor
+	private int golescontra; // Numero de goles en contra
+	private int puntos;      // Numero de puntos
+	
+	public int getVictorias() {
+		
+		
+		return victorias;
+	}
+	public void setVictorias(int victorias) {
+		this.victorias = victorias;
+	}
+	public int getDerrotas() {
+		return derrotas;
+	}
+	public void setDerrotas(int derrotas) {
+		this.derrotas = derrotas;
+	}
+	public int getGolesfavor() {
+		return golesfavor;
+	}
+	public void setGolesfavor(int golesfavor) {
+		this.golesfavor = golesfavor;
+	}
+	public int getGolescontra() {
+		return golescontra;
+	}
+	public void setGolescontra(int golescontra) {
+		this.golescontra = golescontra;
+	}
+	public int getPuntos() {
+		return puntos;
+	}
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+	
+	public String getEquipo() {
+		return equipo;
+	}
+	public void setEquipo(String equipo) {
+		this.equipo = equipo;
+	}
 }
-
-
-//HOLAAAAAA Aquí estamos
